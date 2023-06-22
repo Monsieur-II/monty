6416aa2d79ca3d74d6d8ef2a9cb144db32e2021e
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-06-21 17:21:20 -0700

    initial commit

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+monty_t tool = {NULL, NULL, NULL};
+
+int main(int argc, char **argv)
+{
+	stack_t *head = NULL;
+	unsigned int line_number = 0;
+	char *buffer = NULL;
+	size_t bufsize = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+
+		tool.file = fopen(argv[1], "r");
+		if (!tool.file)
+		{
+			fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
+
+		while (getline(&buffer, &bufsize, tool.file) > 0)
+		{
+			/*tool.buf = buffer*/
+			/*tokenize into tool.args*/
+			/*execute line*/
+		}
+		
---
+++ monty.h
@@ -0,0 +1,65 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+/*Standard library*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/*Macros*/
+#define SIZE 4
+
+/*Structs*/
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * monty_s - extern struct
+ * @file: file stream
+ * @buffer: buffer
+ * @args: tokenized string
+ *
+ * Description - extern struct
+ */
+typedef struct monty_s
+{
+	FILE *file;
+	char *buffer;
+	char *args[SIZE];
+} monty_t;
+
+extern monty_t tool;
+
+/*Functions*/
+
+
+#endif
